d32a9da68ae537c5635dcab4e725bb3114501a1b
DavidLee <78alswo79@gmail.com>
2025-03-22 12:35:39 +0900

    🎉init : 프로젝트 초기 설정

---
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nklcbdty.batch.nklcbdty.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NklcbdtyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NklcbdtyBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nklcbdty.batch.nklcbdty.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NklcbdtyBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
