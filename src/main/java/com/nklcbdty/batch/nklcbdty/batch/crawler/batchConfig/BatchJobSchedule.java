4255c6b598ddf8fe304c50ac01b2bc7e04083e1d
DavidLee <78alswo79@gmail.com>
2025-03-23 16:49:14 +0900

    📝 Chore : 필요없는 임포트소스 삭제

--- src/main/java/com/nklcbdty/batch/nklcbdty/batch/crawler/batchConfig/BatchJobSchedule.java
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/crawler/batchConfig/BatchJobSchedule.java
@@ -1,6 +1,5 @@
 package com.nklcbdty.batch.nklcbdty.batch.crawler.batchConfig;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
