602c159c0825b8791a73cca8abf5f45a935fe702
kingle1024 <teran1024@naver.com>
2025-06-24 09:51:50 +0900

    🚨 Fix: @EnableBatchProcessing 제거 3.x부터는 spring-boot-starter-batch만 있어도 자동 설정이 되기 때문에, @EnableBatchProcessing 어노테이션은 더 이상 자동 설정을 활성화하는 용도가 아니게 됨. 오히려 이 어노테이션은 이제 사용자 정의 배치 설정을 위한 진입점 역할을 하게 됨. 즉, 개발자가 스프링 배치의 기본 자동 설정 외에 특정 설정을 직접 오버라이드하거나 추가하고 싶을 때 사용하는 용도로 바뀜.

--- src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableBatchProcessing
 public class NklcbdtyBatchApplication {
 
 	public static void main(String[] args) {
