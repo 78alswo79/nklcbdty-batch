1e7e46ecc82df9821dfd6359897b22247aa08504
kingle1024 <teran1024@naver.com>
2025-06-25 10:39:41 +0900

    🚨 Fix: 애플리케이션 시작 시간을 줄이기 위해 jobRegistryBeanPostProcessor 빈 정의 제거

---
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/config/CustomBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,22 @@
+package com.nklcbdty.batch.nklcbdty.batch.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) {
+        // jobRegistryBeanPostProcessor 빈 정의를 제거
+        if (registry.containsBeanDefinition("jobRegistryBeanPostProcessor")) {
+            registry.removeBeanDefinition("jobRegistryBeanPostProcessor");
+        }
+    }
+
+    @Override
+    public void postProcessBeanFactory(org.springframework.beans.factory.config.ConfigurableListableBeanFactory beanFactory) {
+        // Nothing to do here
+    }
+
+}
