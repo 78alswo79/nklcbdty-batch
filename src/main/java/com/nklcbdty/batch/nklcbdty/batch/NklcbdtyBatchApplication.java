fd86bba84ab06a10ac7931d4d37d6d50b613a993
kingle1024 <teran1024@naver.com>
2025-06-26 21:32:58 +0900

    🚨 Fix: 불필요 import 제거

--- src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
@@ -1,6 +1,5 @@
 package com.nklcbdty.batch.nklcbdty.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
