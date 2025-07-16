6104313c272e22a81ec26d54632d21b93b9b5438
kingle1024 <teran1024@naver.com>
2025-07-16 16:27:05 +0900

    🚨 Fix: 타임존 한국으로 변경

--- src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
+++ src/main/java/com/nklcbdty/batch/nklcbdty/batch/NklcbdtyBatchApplication.java
@@ -1,5 +1,7 @@
 package com.nklcbdty.batch.nklcbdty.batch;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NklcbdtyBatchApplication {
 
 	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(NklcbdtyBatchApplication.class, args);
 	}
 
